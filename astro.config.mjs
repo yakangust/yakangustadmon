c44a899219209cdb207e79ed97e4323738f9ae3c
Arturo Galeno H <arturo@localhost.localdomain>
2026-08-26 08:07:16 -0600

    Disable session binding and remove wrangler config for Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({
-    imageService: 'passthrough'
+    imageService: 'passthrough',
+    session: false
   })
 });
