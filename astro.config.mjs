841f746e2f5706f38f1b00d0f8e3fc315517997f
Arturo Galeno H <arturo@localhost.localdomain>
2026-08-27 11:32:29 -0600

    Disable KV session and configure clean cloudflare adapter output

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   output: 'server',
   adapter: cloudflare({
-    imageService: 'passthrough'
+    imageService: 'passthrough',
+    session: false
   })
 });
