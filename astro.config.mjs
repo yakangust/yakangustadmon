2e24b0d857814f2d53d701e488a56f46d7ec4971
Arturo Galeno H <arturo@localhost.localdomain>
2026-08-28 02:13:00 -0600

    Set trailingSlash always to resolve /contacto redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,9 @@ import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare({
-    imageService: 'cloudflare',
-    cloudflareModules: true,
-    platformProxy: {
-      enabled: false,
-    },
-  }),
+  adapter: cloudflare(),
+  trailingSlash: 'always',
   build: {
-    format: 'directory',
-  },
+    format: 'directory'
+  }
 });
